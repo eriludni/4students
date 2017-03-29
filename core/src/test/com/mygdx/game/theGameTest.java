23612437277cc3e0598ecca1041457c5c7ddfaba
Niklas Baerveldt <niklas.baerveldt@gmail.com>
2017-03-29 22:39:11 +0200

    Have made the project more compatible with this plugin: https://plugins.jetbrains.com/plugin/3064-junitgenerator-v2-0

---
+++ core/src/test/com/mygdx/game/theGameTest.java
@@ -0,0 +1,65 @@
+package test.com.mygdx.game; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* theGame Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>mar 29, 2017</pre> 
+* @version 1.0 
+*/ 
+public class theGameTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: create() 
+* 
+*/ 
+@Test
+public void testCreate() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: render() 
+* 
+*/ 
+@Test
+public void testRender() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: dispose() 
+* 
+*/ 
+@Test
+public void testDispose() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: hej() 
+* 
+*/ 
+@Test
+public void testHej() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
