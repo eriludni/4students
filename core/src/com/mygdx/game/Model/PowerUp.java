152b307a5e13b8155240dcbdd3c328ad70ca7684
lucas-cr <lucasr@student.chalmers.se>
2017-05-18 13:22:37 +0200

    Added powerup classes

---
+++ core/src/com/mygdx/game/Model/PowerUp.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.Model;
+
+/**
+ * Created by lucasr on 5/18/17.
+ */
+public class PowerUp {
+    private float xPos;
+    private float yPos;
+    private boolean toBeRemoved = false;
+
+    public PowerUp(float xPos, float yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public float getxPos() {
+        return xPos;
+    }
+
+    public float getyPos() {
+        return yPos;
+    }
+
+    public void setToBeRemoved(boolean toBeRemoved) {
+        this.toBeRemoved = toBeRemoved;
+    }
+
+    public boolean getToBeRemoved() {
+        return toBeRemoved;
+    }
+}
