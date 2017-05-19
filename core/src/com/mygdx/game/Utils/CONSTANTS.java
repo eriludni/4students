a097bfbc7b37c1fb0d21debc1f41f799cda4ead8
lucas-cr <lucasr@student.chalmers.se>
2017-05-19 15:30:58 +0200

    Added CONSTANTS

---
+++ core/src/com/mygdx/game/Utils/CONSTANTS.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.Utils;
+
+/**
+ * Created by lucasr on 5/19/17.
+ */
+public class CONSTANTS {
+    //width of the window
+    public static final int WIDTH = 1240;
+    //Height of the window
+    public static final int HEIGHT = 32*20;
+    //Pixels per meter in game
+    public static final float PPM = 100;
+}
