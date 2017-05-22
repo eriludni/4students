58d81c9a5b9e7a059c20f569296308871ae0fc09
lucas-cr <lucasr@student.chalmers.se>
2017-05-22 11:21:47 +0200

    Launcher uses utils instead of Dash

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,12 +3,13 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.Controllers.Dash;
+import com.mygdx.game.Utils.CONSTANTS;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = Dash.WIDTH;
-		config.height = Dash.HEIGHT;
+		config.width = CONSTANTS.WIDTH;
+		config.height = CONSTANTS.HEIGHT;
 		config.resizable = false;
 		new LwjglApplication(new Dash(), config);
 	}
